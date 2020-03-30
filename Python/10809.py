1cd6158df617048a1d30a2bda4e3b0c9f3b59a84
sungwon268 <sungwon268@gmail.com>
2020-03-30 15:42:29 +0900

    알파벳 찾기

--- Python/10809.py
+++ Python/10809.py
@@ -1,11 +1,9 @@
-s = str(input())
-alp_list = [-1 for _ in range(26)]
+s = input()
+word=[-1 for i in range(26)]
 
 for i in s:
-    a = ord(i) - 97
-    if (alp_list[a] == -1):
-        alp_list[a] = s.index(i)
+    word[ord(i)-97]=s.index(i)
 
-for i in alp_list:
-    print(i,end=" ")
+for i in range(len(word)):
+    print(word[i],end=' ')
 
